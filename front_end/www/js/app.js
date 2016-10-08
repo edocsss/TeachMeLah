083e34469a00416428f4e6fe7897746b180c448b
Edwin Candinegara <edwin.candinegara@gmail.com>
2016-10-08 14:46:50 +0800

    Init register page

--- front_end/www/js/app.js
+++ front_end/www/js/app.js
@@ -59,6 +59,5 @@ angular.module('TeachMeLah', [
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
-
+  $urlRouterProvider.otherwise('/register');
 });
--- front_end/www/js/controllers/registerController.js
+++ front_end/www/js/controllers/registerController.js
@@ -2,4 +2,36 @@
 
 angular.module('TeachMeLah').controller('RegisterController', function () {
     var vm = this;
+    var profpicURLs = [
+        'images/profile_pictures/1.png',
+        'images/profile_pictures/2.png',
+        'images/profile_pictures/3.png',
+        'images/profile_pictures/4.png',
+        'images/profile_pictures/5.png'
+    ];
+
+    function getRandomProfpicURL () {
+        var randomIndex = Math.floor(Math.random() * profpicURLs.length);
+        return profpicURLs[randomIndex];
+    }
+
+    vm.newUser = {
+        type: 'tutor',
+        availability: {}
+    };
+
+    vm.registerUser = function () {
+        var userProfile = {
+            name: vm.newUser.fullName,
+            email: vm.newUser.email,
+            contact: vm.newUser.phone,
+            type: vm.newUser.type,
+            profpicURL: getRandomProfpicURL(),
+            tagline: vm.newUser.type === 'tourguide' ? vm.newUser.tagline : null,
+            description: vm.newUser.description === 'tourguide' ? vm.newUser.description : null,
+            location: vm.newUser.type === 'tourguide' ? vm.newUser.location.toLowerCase() : null,
+            price: vm.newUser.type === 'tourguide' ? vm.newUser.price : null,
+            availability: vm.newUser.type === 'tourguide' ? vm.newUser.availability.end : null
+        };
+    };
 });
