20c5d66f30483fc81249ae9b5aff2d7b1c6c6592
Edward Sujono <edwardsujono@yahoo.com>
2016-10-08 13:57:04 +0800

    Express With Path Controller

---
+++ back_end/PathController.js
@@ -0,0 +1,10 @@
+var listJsonReturned = null;
+
+
+module.exports = {
+    initPath : function(router){
+        router.get('/login', function(req, res) {
+
+        });
+    }
+};
--- back_end/index.js
+++ back_end/index.js
@@ -4,10 +4,12 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var http = require("http");
 var router = express.Router();              // get an instance of the express Router
+var pathController = require("./PathController");
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+pathController.initPath(router);
 var server = http.createServer(app);
 server.listen({host : 'localhost',port : 8000});
 console.log("http server listening on %d", 8000);
